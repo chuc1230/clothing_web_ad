f8e3aea23afa5079c68e2d49e9245f2407a402dd
chuc <minhchuc.hhck2003@gmail.com>
2026-01-19 15:40:34 +0700

    19/1/26

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,8 +6,7 @@ import navProfile from '../../assets/nav-profile.svg'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img src={navlogo} alt="" className="nav-logo" />
-        <img src={navProfile} alt="" className="nav-profile" />
+        <h1>Admin</h1>
     </div>
   )
 }
